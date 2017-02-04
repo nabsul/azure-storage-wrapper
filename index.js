656385892e7bc98d57b3806115912f8b6ccd5a53
Nabeel Sulieman <nabeel.sulieman@live.com>
2017-02-03 17:24:14 -0800

    Update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const TableClient = require( './lib/table-client' );
 const util = require( './lib/util' );
+const azureStorage = require( 'azure-storage' );
 
 module.exports = {
 	TableClient,
 	util,
+	azureStorage,
 };
